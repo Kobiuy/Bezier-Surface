1146b842cc795ebb2e926a651728bc11587abd52
Kobiuy <132697518+Kobiuy@users.noreply.github.com>
2024-10-30 13:24:57 +0100

    Loading Points

--- Bezier Surface/Blueprint.cs
+++ Bezier Surface/Blueprint.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Drawing;
 using System.Linq;
+using System.Numerics;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,8 +11,10 @@ namespace Bezier_Surface
 
 	internal class Blueprint
 	{
+		public readonly static string FileName = "BezierSurface.txt";
 		public Bitmap bitmap { get; set; }
 		public PictureBox Canvas { get; set; }
+		public Vector3[] points = new Vector3[16];
 		public Blueprint(int width, int height, PictureBox Canvas)
 		{
 			bitmap = new Bitmap(width, height);
@@ -20,12 +23,25 @@ namespace Bezier_Surface
 			{
 				SetOriginInCenter(g);
 			}
+			LoadPoints();
 		}
 
 		public void SetOriginInCenter(Graphics g)
 		{
 			g.ScaleTransform(1, -1);
 			g.TranslateTransform(Canvas.Width / 2, -Canvas.Height / 2);
+		}
+		public void LoadPoints()
+		{
+			using (var streamReader = new StreamReader(FileName))
+			{
+				int i = 0;
+				while (!streamReader.EndOfStream && i != 16)
+				{
+					string[] line = streamReader.ReadLine().Split();
+					points[i++] = new Vector3(float.Parse(line[0]), float.Parse(line[1]), float.Parse(line[2]));
+				}
+			}
 		}
 	}
 }
